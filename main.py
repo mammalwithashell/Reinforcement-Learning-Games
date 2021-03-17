001236d503841c474d9dc2d99f30d4f5ec1677b9
NikkiM9788 <70670458+NikkiM9788@users.noreply.github.com>
2021-03-17 18:24:47 -0500

    tictactoe source code

--- main.py
+++ main.py
@@ -1,73 +1,145 @@
+import kivy
 from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.behaviors import ToggleButtonBehavior as TB
-from kivy.config import Config
-from kivy.utils import platform 
-from kivy.properties import StringProperty
-from kivy.uix.popup import Popup 
-from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+import random as rand
+from os import system
+from collections import defaultdict
 
-""" 
-Builder is used to load in the design .kv files
 
-platform is used to determine the os:
-    A string identifying the current operating system. It is one of: ‘win’, ‘linux’, ‘android’, ‘macosx’, ‘ios’ or ‘unknown’.
-"""
+class MyGridLayout(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGridLayout, self).__init__(**kwargs)
 
-# Desktop config
-if platform in ["win", "macosx", "linux"]:
-    Config.set('graphics', 'resizable', False)
-    # fix the width and height of the window  
-    Config.set('graphics', 'width', '500') 
-    Config.set('graphics', 'height', '500') 
-    
-# Mobile config
-if platform in ["ios","android"]:
-    pass
-    
+        self.cols = 3
+        self.rows = 5
 
-# Import the Screens for the individual games
-from game_logic.tiktactoe import TicTacToeScreen
-from game_logic.connect4 import Connect4Screen
-from game_logic.dotsandboxes import DotsAndBoxesScreen
+        #add widgets
+        self.backButton = Button(text='back button', font_size=20)
+        self.add_widget(self.backButton)
 
-# Load in gui 
-Builder.load_file("design/gui.kv")
-Builder.load_file("design/tiktactoe.kv")
-Builder.load_file("design/connect4.kv")
-Builder.load_file("design/dotsandboxes.kv")
+        self.scores = Label(text='scores')
+        self.add_widget(self.scores)
 
-class TitleScreen(Screen):
-    diff_choice = StringProperty()
-    game_choice = StringProperty()
-    match_style = StringProperty()
-    
-    def load_game(self):
-        """Function to swap to the game screen and pass the game variables to the appropriate screen. Displays an error message if any of the settings are not selected
-        """
-        # Show error message if any of the toggles are not picked        
-        if not self.diff_choice or not self.game_choice or not self.match_style:
-            content = Button(text="Dismiss")
-            error = Popup(title="Select one of each option", content=content, size=(40, 60))
-            content.bind(on_press=error.dismiss)
-            error.open()
-            return
+        self.quitButton = Button(text='quit button', font_size=20)
+        self.add_widget(self.quitButton)
+
+        self.square1 = Button(text="1", font_size=72)
+        #add function to the button
+        self.square1.bind(on_press=self.press1)
+        self.add_widget(self.square1)
+
+        self.square2 = Button(text="2", font_size=72)
+        self.square2.bind(on_press=self.press2)
+        self.add_widget(self.square2)
+
+        self.square3 = Button(text="3", font_size=72)
+        self.square3.bind(on_press=self.press3)
+        self.add_widget(self.square3)
+
+        self.square4 = Button(text="4", font_size=72)
+        self.square4.bind(on_press=self.press4)
+        self.add_widget(self.square4)
         
-        # load game settings and swap screens
-        game_screen = self.manager.get_screen(self.game_choice)
-        game_screen.load_settings(self.diff_choice, self.match_style)
-        self.manager.current = self.game_choice
+        self.square5 = Button(text="5", font_size=72)
+        self.square5.bind(on_press=self.press5)
+        self.add_widget(self.square5)
 
+        self.square6 = Button(text="6", font_size=72)
+        self.square6.bind(on_press=self.press6)
+        self.add_widget(self.square6)
+
+        self.square7 = Button(text="7", font_size=72)
+        self.square7.bind(on_press=self.press7)
+        self.add_widget(self.square7)
+        
+        self.square8 = Button(text="8", font_size=72)
+        self.square8.bind(on_press=self.press8)
+        self.add_widget(self.square8)
+
+        self.square9 = Button(text="9", font_size=72)
+        self.square9.bind(on_press=self.press9)
+        self.add_widget(self.square9)
+
+        self.blankspot = Label(text='')
+        self.add_widget(self.blankspot)
+
+        self.blankspot = Label(text='message box')
+        self.add_widget(self.blankspot)
+
+        self.blankspot = Label(text='')
+        self.add_widget(self.blankspot)
+
+        self.count = 1
+
+    def alternate_turn(self):
+        self.count = self.count + 1
+        self.turn = self.count%2
     
-class RootWidget(ScreenManager):
-    pass
+    def press1(self, instance):
+        self.alternate_turn()
+        if self.turn == 0:
+            self.square1.text = 'X'
+        else:
+            self.square1.text = 'O'
+    def press2(self, instance):
+        self.alternate_turn()
+        if self.turn == 0:
+            self.square2.text = 'X'
+        else:
+            self.square2.text = 'O'
+    def press3(self, instance):
+        self.alternate_turn()
+        if self.turn == 0:
+            self.square3.text = 'X'
+        else:
+            self.square3.text = 'O'
+    def press4(self, instance):
+        self.alternate_turn()
+        if self.turn == 0:
+            self.square4.text = 'X'
+        else:
+            self.square4.text = 'O'
+    def press5(self, instance):
+        self.alternate_turn()
+        if self.turn == 0:
+            self.square5.text = 'X'
+        else:
+            self.square5.text = 'O'
+    def press6(self, instance):
+        self.alternate_turn()
+        if self.turn == 0:
+            self.square6.text = 'X'
+        else:
+            self.square6.text = 'O'
+    def press7(self, instance):
+        self.alternate_turn()
+        if self.turn == 0:
+            self.square7.text = 'X'
+        else:
+            self.square7.text = 'O'
+    def press8(self, instance):
+        self.alternate_turn()
+        if self.turn == 0:
+            self.square8.text = 'X'
+        else:
+            self.square8.text = 'O'
+    def press9(self, instance):
+        self.alternate_turn()
+        if self.turn == 0:
+            self.square9.text = 'X'
+        else:
+            self.square9.text = 'O'
+# ----------kivy--------------------------------------------------------------------------------------------------------------------------
 
-class GameApp(App):
-    title = "Reinforcement Learning Game"
+ # ----------kivy--------------------------------------------------------------------------------------------------------------------------
+class Tictactoe(App):
     def build(self):
-        return RootWidget()
+        return MyGridLayout()
 
-if __name__ == '__main__':
-    GameApp().run()
+if __name__ == "__main__":
+    Tictactoe().run()
+# ----------kivy--------------------------------------------------------------------------------------------------------------------------
+
