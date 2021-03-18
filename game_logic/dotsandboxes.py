88b4f279d55da7eb4fa52dec6dde1bae099715b4
james <james.peters97@outlook.com>
2021-03-17 20:52:32 -0500

    resizeable dots & menu stuff on d&b game

--- game_logic/dotsandboxes.py
+++ game_logic/dotsandboxes.py
@@ -10,15 +10,12 @@ class DotsAndBoxesScreen(Screen):
     difficulty_setting = StringProperty("")
     match = StringProperty("")
     turn = BooleanProperty(True)
-    dots = ListProperty()
+    
+    #dots = [self.game_grid.canvas.get_group(f"dot{i}") for i in range(9)]
+    
     def setup(self):
         print(Window.size)
-        with self.game_grid.canvas:
-            # Ellipse(pos=(30,30), size=(10,10))
-            dot_radius = 10
-            for x in range(20, 481, 230):
-                for y in range(20, 421, 200):
-                    self.dots.append(Ellipse(pos=(x - dot_radius, y - dot_radius), size=(dot_radius, dot_radius)))
+
             
     def menu(self):
         self.manager.transition = SlideTransition(direction="right")
@@ -30,11 +27,7 @@ class DotsAndBoxesScreen(Screen):
     
     def on_touch_down(self, touch):
         print(touch.x, touch.y)
-        if touch.x > 20 and touch.x < 250:
-            with self.game_grid.canvas:
-                Line(pos=(20, 20, 250, 20))
         return super().on_touch_down(touch)
     
     def on_size(self, instance, value):
-        if self.manager.current == self.name:
-            self.dots[0].pos = (40, 40)
+        self.game_grid.clear_widgets()
