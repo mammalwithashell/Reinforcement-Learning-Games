2b762e87c0663181d35f30af32576f562a8f527a
Nikki Meyer <marymeyer@my.unt.edu>
2021-03-30 13:08:50 -0500

    moved kivy over to gridlayout, added atables, playable tictactoe Co-authored-by: Brian Wesley Little <59803204+Balkito@users.noreply.github.com>

--- game_logic/connect4.py
+++
@@ -1,8 +0,0 @@
-from kivy.uix.screenmanager import ScreenManager, Screen
-
-class Connect4Screen(Screen):
-    def load_settings(self, diff, match):
-        pass
-    
-    def menu(self):
-        self.manager.current = "title"
---
+++ game_logic/tictactoeAI/BoardEnvironment.py
@@ -0,0 +1,94 @@
+import random as rand
+from os import system
+from collections import defaultdict
+
+class BoardEnvironment:
+
+    def __init__(self):
+        "init board"
+
+    def set_players(self, AI):
+        self.AI = AI
+        self.reset()
+
+    def reset(self):
+        self.turn = 'X'
+
+        self.board = list('---------')
+        if(rand.random() < 0.5):
+            self.current_player = True
+        else:
+            self.current_player = False
+
+        return self.current_player
+
+    def print_board(self, board_string = None):
+        if not board_string:
+            B = self.board
+        else:
+            B = board_string
+        check_for = ['X', 'O']
+        print(B[0] if B[0] in check_for else 1,'|', B[1] if B[1] in check_for else 2,'|', B[2] if B[2] in check_for else 3, sep='')
+        print('-----')
+        print(B[3] if B[3] in check_for else 4,'|', B[4] if B[4] in check_for else 5,'|', B[5] if B[5] in check_for else 6, sep='')
+        print('-----')
+        print(B[6] if B[6] in check_for else 7,'|', B[7] if B[7] in check_for else 8,'|', B[8] if B[8] in check_for else 9, sep='')
+
+    def get_state(self):
+        return "".join(self.board)
+
+    def other_player(self):
+        return not self.current_player
+
+    def available_actions(self, first):
+        return [ind for ind, val in enumerate(self.board) if val == '-']
+
+    def play_game(self):
+        self.reset()
+        while( not self.is_full() ):
+            system('clear')
+            if( not self.current_player ):
+                choice = self.AI.select_action(None)
+            else:
+                self.print_board()
+                choices = self.available_actions(None)
+                print("Select your space to play. Your pieces are", self.turn + '.', "Current choices are")
+                print(list(x+1 for x in choices))
+                choice = 10
+                while(choice not in choices):
+                    choice = input()
+                    choice = int(choice) - 1
+                    if(choice not in choices):
+                        print("Spot not available. Current choices are")
+                        print(list(x+1 for x in choices))
+
+            self.board[choice] = self.turn
+
+            if self.winner(self.turn):
+                system('clear')
+                if(self.current_player):
+                    print("You won!")
+                else:
+                    print("You lost!")
+                self.print_board()
+                return self.current_player
+
+            self.turn = 'X' if self.turn == 'O' else 'O'
+            self.current_player = not self.current_player
+        system('clear')
+        self.print_board()
+        print("Tie!")
+
+        return None
+
+    def winner(self, check_for = ['X', 'O']):
+        straight_lines = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),
+                          (1,4,7),(2,5,8),(0,4,8),(2,4,6))
+        for turn in check_for:
+            for line in straight_lines:
+                if all(x == turn for x in (self.board[i] for i in line)):
+                    return turn
+        return ''
+
+    def is_full(self):
+        return('-' not in self.board)
